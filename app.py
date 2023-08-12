52bd7cc599fc598b02dbcff3e8e7302b95d954e8
David Warutumo <davidwarchy@gmail.com>
2023-08-12 06:26:25 +0300

    input sentence as a command line argument

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import sys
 import en_core_web_sm
 from rules import rule1, rule2
 
 nlp = en_core_web_sm.load()
 
 if __name__ == '__main__':
-    text = """KQ signs codeshare agreement with Delta - Capital Business"""
+    if len(sys.argv) < 2:
+        print("Please provide a sentence as a command-line argument.")
+        sys.exit(1)
+
+    text = ' '.join(sys.argv[1:])
 
     # triple with rule 1
     result = rule1(nlp, text)
@@ -13,5 +18,3 @@ if __name__ == '__main__':
     # triple with rule 2 (gpt3.5)
     result = rule2(text)
     print(f'RULE 2: {result}')
-
-    
